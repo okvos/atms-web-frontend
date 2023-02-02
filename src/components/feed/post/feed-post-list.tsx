ba5a585e0db521a750fa32ce33393f88edb30ff0
okvos <72706419+okvos@users.noreply.github.com>
2023-02-02 09:56:02 -0500

    Add post list div

---
+++ src/components/feed/post/feed-post-list.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Post } from "../../../util/api/models/feed/Post";
+import { FeedPost } from "./feed-post";
+import "./feed-list.css";
+
+type FeedPostListType = {
+  posts: Post[];
+};
+export function FeedPostList({ posts }: FeedPostListType) {
+  return (
+    <div className="feed-list">
+      {posts.map((post, key) => {
+        return <FeedPost key={key} post={post} />;
+      })}
+    </div>
+  );
+}
