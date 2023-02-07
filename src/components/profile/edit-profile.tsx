5dcbc757de2adc2b4f9132586ce8ba764ed970e1
okvos <72706419+okvos@users.noreply.github.com>
2023-02-07 07:51:36 -0500

    Implement uploading header images [sc-88] (#17)

--- src/components/profile/edit-profile.tsx
+++ src/components/profile/edit-profile.tsx
@@ -14,17 +14,41 @@ import { FormControl } from "baseui/form-control";
 import { FileUploader } from "baseui/file-uploader";
 import { PutUserProfileRequest } from "@atms/api/request/user/profile";
 import { toast } from "@atms-modules";
+import { uploadImage } from "@atms/modules/uploads";
+import { Profile } from "@atms/api/models";
 
 export function EditProfile({
   updateProfileInfo,
+  profile,
 }: {
-  updateProfileInfo: (key: string, value: string) => void;
+  updateProfileInfo: (
+    key: string,
+    value: string,
+    headerImageKey: string
+  ) => void;
+  profile: Profile;
 }) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [updatedBio, setUpdatedBio] = useState<string>("");
-  const [updatedDisplayName, setUpdatedDisplayName] = useState<string>("");
+  const [updatedBio, setUpdatedBio] = useState<string>(profile.bio);
+  const [updatedDisplayName, setUpdatedDisplayName] = useState<string>(
+    profile.username
+  );
+  const [updatedHeaderKey, setUpdatedHeaderKey] = useState<string>(
+    profile.header_image_url
+  );
 
   const [isUpdating, setIsUpdating] = useState<boolean>(false);
+
+  const [isHeaderUploaded, setIsHeaderUploaded] = useState<boolean>(false);
+
+  async function uploadHeaderImage(files: File[]) {
+    const headerKey = await uploadImage(files[0]);
+    if (!headerKey) {
+      return toast("error", "Header not uploaded. Please try another image.");
+    }
+    setUpdatedHeaderKey(headerKey);
+    setIsHeaderUploaded(true);
+  }
 
   async function saveChanges() {
     setIsUpdating(true);
@@ -33,11 +57,11 @@ export function EditProfile({
       const req = new PutUserProfileRequest({
         bio: updatedBio,
         display_name: updatedDisplayName,
-        header_image_url: "",
+        header_image_url: updatedHeaderKey,
       });
       await req.execute();
       toast("success", "Your profile has been updated");
-      updateProfileInfo(updatedDisplayName, updatedBio);
+      updateProfileInfo(updatedDisplayName, updatedBio, updatedHeaderKey);
     } catch (e: any) {
       toast("error", e.message);
     } finally {
@@ -66,6 +90,7 @@ export function EditProfile({
               onChange={(e) => setUpdatedDisplayName(e.target.value)}
               placeholder="Display name"
               clearOnEscape
+              value={updatedDisplayName}
             />
           </FormControl>
 
@@ -74,11 +99,27 @@ export function EditProfile({
               onChange={(e) => setUpdatedBio(e.target.value)}
               placeholder="Bio"
               clearOnEscape
+              value={updatedBio}
             />
           </FormControl>
 
-          <FormControl label="Header Image">
-            <FileUploader />
+          <FormControl
+            label="Header Image"
+            caption="Change your existing header image"
+          >
+            <FileUploader
+              onDrop={uploadHeaderImage}
+              onCancel={() => {
+                setUpdatedHeaderKey(profile.header_image_url);
+                setIsHeaderUploaded(false);
+              }}
+              multiple={false}
+              accept={["image/png", "image/jpeg", "image/gif"]}
+              progressAmount={isHeaderUploaded ? 100 : undefined}
+              progressMessage={
+                isHeaderUploaded ? "Uploaded! Don't forget to save." : undefined
+              }
+            />
           </FormControl>
         </ModalBody>
         <ModalFooter>
---
+++ src/modules/uploads/index.ts
@@ -0,0 +1,56 @@
+import { toast } from "@atms/modules/notifications/toast";
+import {
+  ImageType,
+  PostUploadImageRequest,
+} from "@atms/api/request/upload-image";
+
+function readFileAsync(file: File) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+
+    reader.onerror = reject;
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+export async function uploadImage(imageFile: File): Promise<string> {
+  let file = <ArrayBuffer>await readFileAsync(imageFile);
+  let arr = new Uint8Array(file).subarray(0, 4);
+  let header = "";
+  let imageType;
+  for (let i = 0; i < arr.length; i++) {
+    header += arr[i].toString(16);
+  }
+  switch (header) {
+    case "89504e47":
+      imageType = ImageType.PNG;
+      break;
+    case "47494638":
+      imageType = ImageType.GIF;
+      break;
+    case "ffd8ffe0":
+    case "ffd8ffe1":
+    case "ffd8ffe2":
+    case "ffd8ffe3":
+    case "ffd8ffe8":
+      imageType = ImageType.JPG;
+      break;
+    default:
+      toast("error", "Invalid image type. Only PNG, JPG, or GIF are allowed.");
+      return "";
+  }
+
+  let req = new PostUploadImageRequest({ image_type: imageType });
+  let resp = await req.execute();
+
+  await fetch(resp.url, {
+    method: "PUT",
+    body: file,
+  });
+
+  return resp.key;
+}
