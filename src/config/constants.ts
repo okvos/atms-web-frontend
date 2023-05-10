0fad9f736fd5c9435c12bc4f05265d9a105acf97
okvos <72706419+okvos@users.noreply.github.com>
2023-05-10 00:46:56 -0400

    Update api url for testing

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = "atmosphere";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "http://local.atmospheretest.site:8080";
 export const LOCALSTORAGE_KEY = "atms-user";
 export const UPLOADS_URL = "https://uploads.atmospheretest.site"
